0732bb0eb32f26a638b3f812d88fb7bddc43efeb
st18181ys <st18181ys@gm.ibaraki-ct.ac.jp>
2021-12-01 08:22:09 +0900

    week10

---
+++ auto.py
@@ -0,0 +1,25 @@
+from automata.fa.dfa import DFA
+
+dfa = DFA(
+    states={'q0', 'q1', 'q2'},
+    input_symbols={'0', '1'},
+    transitions={
+        'q0': {'0': 'q0', '1': 'q1'},
+        'q1': {'0': 'q0', '1': 'q2'},
+        'q2': {'0': 'q2', '1': 'q1'}
+    },
+    initial_state='q0',
+    final_states={'q1'}
+)
+
+r = dfa.read_input('01')
+print(f"read_input: {r}")
+
+if dfa.accepts_input("01100101001011111111111"):
+    print('accepted')
+else:
+    print('rejected')
+
+r = dfa.read_input_stepwise('0111')
+for i in r:
+    print(f"{i}")
