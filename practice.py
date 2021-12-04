e7e80be8539656c3ad28a502a293080e347c47b8
st18181ys <st18181ys@gm.ibaraki-ct.ac.jp>
2021-12-04 19:46:41 +0900

    scatter

--- practice.py
+++ practice.py
@@ -1,9 +1,25 @@
-import numpy as np
 import matplotlib.pyplot as plt
-
-means=[1,2,3,4,5,6,7,8,9,10]
-
-x=np.arange(len(means))
-
-plt.bar(x,means,width=0.5)
+ 
+# x、yデータ
+x = range(20)
+y = range(20)
+ 
+# 点(x, y)がもつ量
+z = range(20)
+ 
+# カラーマップ
+cm = plt.cm.get_cmap('Blues')
+# figureを生成する
+fig = plt.figure()
+ 
+# axをfigureに設定する
+ax = fig.add_subplot(1, 1, 1)
+ 
+# axに散布図を描画、戻り値にPathCollectionを得る
+mappable = ax.scatter(x, y, c=z, vmin=0, vmax=100, s=35, cmap=cm)
+ 
+# カラーバーを付加
+fig.colorbar(mappable, ax=ax)
+ 
+# 表示
 plt.show()
