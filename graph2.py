d040a6e661f19232a5a2b3fbd9b907b9c99d7ad3
st18181ys <st18181ys@gm.ibaraki-ct.ac.jp>
2021-12-24 14:52:32 +0900

    kk

--- graph2.py
+++ graph2.py
@@ -1,14 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from database import database
-
-db=database()
 
 left=np.array([1,2,3,4,5])
-height=np.array([100,200,500,300,400])
-plt.title(" People ")
-plt.plot(left,height,marker="o",color="black",linestyle="dashdot")
-plt.xlabel("time")
-plt.ylabel("number")
-plt.grid(True)
+plt.pie(left)
 plt.show()
